9023b7dac645a8b3a27b53aeb7e69bba600b450b
o4villegas <lando@GVOassurancepartners.com>
2025-06-15 18:29:32 -0400

    Update react-router.config.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,7 +2,14 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: true,
+  serverBuildFile: "index.js",
+  serverConditions: ["workerd", "worker", "browser"],
+  serverDependenciesToBundle: "all",
+  serverMainFields: ["browser", "module", "main"],
+  serverMinify: true,
+  serverModuleFormat: "esm",
+  serverPlatform: "neutral",
   future: {
-    unstable_viteEnvironmentApi: true,
+    unstable_singleFetch: true,
   },
 } satisfies Config;
